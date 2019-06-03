ebc8e3e754bec1a51e390ef6b1a82414534d8dda
AoiHosizora <a970335605@hotmail.com>
2019-06-03 21:06:55 +0800

    Add Github & Adjust Layout

--- getData.py
+++ getData.py
@@ -3,19 +3,49 @@ from lxml import etree
 
 from enum import Enum
 
-class Data(Enum):
-	AllData = 0
-	DailyData = 1
-	MonthlyData = 2
+class WebSite(Enum):
+	Gitee = 0
+	Github = 1
+	Csdn = 2
+
 
 GiteeURL = 'https://gitee.com/explore/starred'
+GithubURL = 'https://github.com/trending?since=daily'
 
-def getData():
+def getData(opt):
 	'''
-	获取 Gitee 热门仓库的名称和描述
+	获取:
+	Gitee / Github 热门仓库的名称和描述
+	Csdn 热门帖标题
 	'''
-	html = requests.get(GiteeURL)
-	et = etree.HTML(html.text)
-	title = et.xpath('//a[@class="title project-namespace-path"]/text()')
-	desc = et.xpath('//div[@class="project-desc"]/text()')
-	return len(title), title, desc
+	
+	def removeSpAndNull(l):
+		'''
+		去除 List 内的空字符串与空项
+		'''
+		l = [li.strip() for li in l]
+		while '' in l:
+			l.remove('')
+		return l
+
+	if opt == WebSite.Gitee:
+		html = requests.get(GiteeURL)
+		et = etree.HTML(html.text)
+		title = et.xpath('//a[@class="title project-namespace-path"]/text()')
+		desc = et.xpath('//div[@class="project-desc"]/text()')
+		return len(title), title, desc
+
+	elif opt == WebSite.Github:
+		html = requests.get(GithubURL)
+		et = etree.HTML(html.text)
+
+		title = et.xpath('//li[@class="col-12 d-block width-full py-4 border-bottom"]/div[@class="d-inline-block col-9 mb-1"]//a/text()')
+		title = removeSpAndNull(title)
+
+		desc = et.xpath('//li[@class="col-12 d-block width-full py-4 border-bottom"]/div[@class="py-1"]//p/text()')
+		desc = removeSpAndNull(desc)
+		return len(title), title, desc
+		
+	elif opt == WebSite.Casn:
+		pass
+	
--- main.py
+++ main.py
@@ -4,23 +4,32 @@ from wordcloud import WordCloud
 
 import sys
 import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
 
 font = {
 	'family' : 'SimHei',
 	'weight' : 'regular',
-	'size'   : '12'
+	'size'   : '11'
 }
 plt.rc('font', **font)
 plt.rc('axes', unicode_minus=False)
-Axis_FontSize = 14
+
+SupTitle_Size = 18
+
+BarVal_FontSize = 10
+Bar_Rot = 75
+
 WC_Height = 500
 WC_Width = 2000
 
-def analyse(tokens, tokencnt, percnt):
+BarPar_Color = 'rgbycm' # bgrcmykw
+
+def analyse(tokens, tokencnt, percnt, title):
 	'''
 	分析 tokens 列表生成
-	柱状图, 圆饼图, 词云图
+	词云图，柱状图, 圆饼图
 	'''
+	gs = gridspec.GridSpec(2, 3)
 	src = {}
 	for t in tokens:
 		if src.get(t) == None:
@@ -30,6 +39,10 @@ def analyse(tokens, tokencnt, percnt):
 	
 	src = dict(sorted(src.items(), key=lambda v: (v[1], v[0]), reverse=True)[0: tokencnt])
 	print(src)
+	
+	# 新页面
+	plt.figure()
+	plt.suptitle(title, fontsize = SupTitle_Size)
 
 	# wordcloud
 	text = " ".join(tokens) # src.keys()
@@ -39,7 +52,7 @@ def analyse(tokens, tokencnt, percnt):
 		width = WC_Width, height = WC_Height
 	).generate(text)
 
-	plt.subplot(2, 1, 1)
+	ax = plt.subplot(gs[0, :])
 	plt.title('词云图统计')
 	plt.axis('off')
 	plt.imshow(wordcloud, interpolation="bilinear")
@@ -48,11 +61,18 @@ def analyse(tokens, tokencnt, percnt):
 	x_axis = list(src.keys())
 	y_axis = list(src.values())
 
-	plt.subplot(2, 2, 3)
+	plt.subplot(gs[1, 0:2])
 	plt.title('柱状图统计')
-	plt.ylabel(u'次数', fontsize=Axis_FontSize)
-	plt.bar(x_axis, y_axis, color='rgby')
-	plt.xticks(x_axis, x_axis, rotation=60)
+	plt.ylabel(u'次数')	
+
+	xy = plt.gca() # get current axis
+	xy.spines['right'].set_color('none')
+	xy.spines['top'].set_color('none')
+
+	plt.bar(x_axis, y_axis, color=BarPar_Color)
+	plt.xticks(x_axis, x_axis, rotation=Bar_Rot)
+	for x, y in zip(x_axis, y_axis):
+		plt.text(x, y + .1, str(y), ha='center', fontsize=BarVal_FontSize)
 
 	# pie
 	labels = list(src.keys())[0: percnt]
@@ -63,17 +83,30 @@ def analyse(tokens, tokencnt, percnt):
 	pers = pers_all[0: percnt]
 	pers.append(sum(pers_others))
 
-	plt.subplot(2, 2, 4)
+	plt.subplot(gs[1, 2])
 	plt.title('圆饼图统计')
-	plt.pie(pers, labels=labels, colors='rgby', autopct='%2.0f%%', shadow=False, startangle=90, pctdistance=0.6)
+	plt.pie(pers, labels=labels, colors=BarPar_Color, autopct='%2.0f%%', shadow=False, startangle=90, pctdistance=0.6)
+	# analyse(tokens, tokencnt, percnt) end
 
-	plt.show()
-
-if __name__ == "__main__":
-	cnt, title, desc = data.getData()
+def getDataAndToken(opt):
+	'''
+	获取爬虫出的已经处理好的 token list
+	'''
+	cnt, title, desc = data.getData(opt)
 	d_tokens = []
 	for t, d in zip(title, desc):
 		d_nopunc = token.HandlePunctuation(d)
 		d_tokens += token.getToken(d_nopunc)
+	return d_tokens
 
-	analyse(d_tokens, 20, 15)
+if __name__ == "__main__":
+	gitee_list = getDataAndToken(data.WebSite.Gitee)
+	github_list = getDataAndToken(data.WebSite.Github)
+
+	tokencnt = 35 # 使用的 Token 总数
+	percnt = 15 # 圆饼图显示的块数
+
+	analyse(gitee_list, tokencnt, percnt, 'Gitee 热门词汇统计')
+	analyse(github_list, tokencnt, percnt, 'Github 热门词汇统计')
+	
+	plt.show()
